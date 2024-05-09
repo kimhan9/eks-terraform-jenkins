6a77983410bacab247cc3853cade6cb43851fdef
Kim Hang <55677067+kimhan9@users.noreply.github.com>
2024-05-09 17:17:45 +0800

    Update main.tf

--- jenkins_server/main.tf
+++ jenkins_server/main.tf
@@ -68,7 +68,7 @@ module "ec2_instance" {
   name = "Jenkins-Server"
 
   instance_type               = var.instance_type
-  key_name                    = "jenkins-server-key"
+  key_name                    = "kim2"
   monitoring                  = true
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
